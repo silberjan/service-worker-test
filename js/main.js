afea800b932bff8a6f1857fd5bc8ed1f82f23959
jan.ladleif <jan.ladleif@student.hpi.de>
2016-05-12 14:36:00 +0200

    bit of infrastructure for observing video requests

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 if ('serviceWorker' in navigator) {
-	console.log('Service Worker is supported');
 	navigator.serviceWorker.register('/sw.js').then(function(reg) { // sw.js muss in app root
 		console.log(reg);
 	}).catch(function(err) {
--- sw.js
+++ sw.js
@@ -4,30 +4,51 @@ this.importScripts("/js/localforage.js");
 
 // Service worker install event
 this.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/js/main.js',
-      ]);
-    })
-  );
-  console.log('Installed', event);
+	event.waitUntil(
+		caches.open('v1').then((cache) => {
+			return cache.addAll([
+				'/',
+				'/index.html',
+				'/js/main.js'
+			]);
+		})
+	);
+	console.log('Installed', event);
 });
 
 function handleUncachedRequest(request) {
-  return fetch(request);
+	return fetch(request);
 };
 
+function handleVideo(event) {
+	return fetch(event.request);
+	
+	// do something
+	fetch(event.request).then(function(response) {
+		console.log("Video Response", response);
+	});
+}
+
 this.addEventListener('fetch',(event) => {
-
-  event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || handleUncachedRequest(event.request);
-      }).catch(() => {
-        return Response('fallback');
-      })
-  );
-
+	
+	// handle videos differently
+	if (event.request.url.indexOf('.mp4') >= 0) {
+		console.log("Video (Range "+event.request.headers.get('range')+")", event.request);
+		return handleVideo(event);
+	}
+	else {
+		event.respondWith(
+				caches.match(event.request).then((response) => {
+					if (response) {
+						console.log("Cache Hit", event.request);
+						return response;
+					}
+					console.log("Cache Miss", event.request);
+					return handleUncachedRequest(event.request);
+				}).catch(() => {
+					console.log("Fallback", event.request);
+					return Response('fallback');
+				})
+		);
+	}
 });
