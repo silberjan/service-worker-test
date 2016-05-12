b19c90cea2469605e3e414834c82241953c75b47
jan.ladleif <jan.ladleif@student.hpi.de>
2016-05-12 16:29:37 +0200

    videos are now stored in the local storage by the service worker

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js').then(function(reg) { // sw.js muss in app root
-		console.log(reg);
+		//console.log(reg);
 	}).catch(function(err) {
 		console.log(err);
 	});
--- sw.js
+++ sw.js
@@ -20,13 +20,21 @@ function handleUncachedRequest(request) {
 	return fetch(request);
 };
 
-function handleVideo(event) {
-	return fetch(event.request);
-	
-	// do something
-	fetch(event.request).then(function(response) {
-		console.log("Video Response", response);
+function handleVideo(event) {	
+	// get the video and store it in localforage
+	var request = new Request(event.request.url);
+	fetch(request)
+		.then((response) => {
+			return response.blob();
+		})
+		.then((blob) => {
+			localforage.setItem(event.request.url, blob, function (item, err) {
+			console.log("Finished downloading and storing video.", blob);
+		});
 	});
+	
+	// return the normal video from the server
+	return fetch(event.request);
 }
 
 this.addEventListener('fetch',(event) => {
@@ -34,7 +42,7 @@ this.addEventListener('fetch',(event) => {
 	// handle videos differently
 	if (event.request.url.indexOf('.mp4') >= 0) {
 		console.log("Video (Range "+event.request.headers.get('range')+")", event.request);
-		return handleVideo(event);
+		event.respondWith(handleVideo(event));
 	}
 	else {
 		event.respondWith(
@@ -47,7 +55,7 @@ this.addEventListener('fetch',(event) => {
 					return handleUncachedRequest(event.request);
 				}).catch(() => {
 					console.log("Fallback", event.request);
-					return Response('fallback');
+					return new Response('fallback');
 				})
 		);
 	}
